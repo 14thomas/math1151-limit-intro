d764be018b9a1c5f644d8499310e5b3283783fff
Thomas <z5477811@ad.unsw.edu.au>
2024-12-05 04:48:39 +1100

    added scene 1

--- limit.py
+++ limit.py
@@ -9,37 +9,65 @@ class IntroScene(Scene):
     def construct(self):
         title = Text("Introduction to Limits", font_size=48, color=custom_color)
         self.play(Write(title))
-        self.wait(0.7)
+        self.wait(5)
         self.play(FadeOut(title))
 
+        # Original equation
+        title2 = MathTex(r"f(x) = \frac{x^2 + 2}{x^2 + 1}").scale(0.8)
+        self.play(Write(title2))
+        self.wait(3)
+
+        # Move the equation to a target position
+        target_position = title2.copy()
+        target_position.to_edge(UP + RIGHT)
+        target_position.shift(LEFT + DOWN)
+        self.play(Transform(title2, target_position), run_time=0.9)
+        self.wait(1)
+        
         plane = NumberPlane(
             x_range=[-10, 10, 2],
-            y_range=[-10, 10, 2],
+            y_range=[-10, 10, 1],
             background_line_style={
                 "stroke_color": BLUE_D,
                 "stroke_width": 2,
-                "stroke_opacity": 0.6
+                "stroke_opacity": 0
             }
         )
+        plane.add_coordinates()
+
         self.play(FadeIn(plane))
 
-        cubic_function = lambda x: 0.5 * (x - 1) * (x - 3) * (x + 1)
+        cubic_function = lambda x: (x ** 2 + 2) / (x ** 2 + 1)
+        curve = plane.plot(cubic_function, color=custom_color, x_range=[-10, 10]) 
+        self.play(FadeIn(curve))
 
-        limit_x = 2.1547
-
-        cubic_curve_before = plane.plot(cubic_function, color=custom_color, x_range=[-10, limit_x - 0.1]) 
-        cubic_curve_after = plane.plot(cubic_function, color=custom_color, x_range=[limit_x + 0.1, 10])
-
-        limit_point = plane.c2p(limit_x, cubic_function(limit_x))
-        limit_dot = Circle(radius=0.1, color=custom_color, fill_opacity=0)
-        limit_dot.move_to(limit_point)
-
-        start_x = -0.81
+        start_x = 0.3
+        limit_x = 8
         moving_dot = Dot(plane.c2p(start_x, cubic_function(start_x)), color=custom_pink)
-        self.play(FadeIn(cubic_curve_before, cubic_curve_after, limit_dot, moving_dot))
-
+        self.play(FadeIn(moving_dot))
         path = plane.plot(cubic_function, x_range=[start_x, limit_x], color=custom_pink)
         
-        self.play(MoveAlongPath(moving_dot, path, rate_func=rush_from), run_time=5)
+        y_line = DashedLine(
+            start=plane.c2p(-10, 1),
+            end=plane.c2p(10, 1),
+            color=custom_light,
+            dash_length=0.2
+        )
+        self.play(FadeIn(y_line))
+
+        self.play(MoveAlongPath(moving_dot, path, rate_func=linear), run_time=5.5)
+
+
+
+
+        eqn2 = MathTex(r"f(\infty) = \frac{\infty^2 + 2}{\infty^2 + 1}").scale(0.8).move_to(title2)
+        eqn3 = MathTex(r"f(\infty) = 1").scale(0.8).move_to(title2)
+
+        self.play(ReplacementTransform(title2, eqn2), run_time=1)
+        self.wait(1)
+        self.play(ReplacementTransform(eqn2, eqn3), run_time=1)
+        self.wait(1)
+        question_mark = MathTex(r"?").scale(0.8).next_to(eqn3, DOWN, buff=0.3)
+        self.play(Write(question_mark), run_time=1)
 
         self.wait(2)
